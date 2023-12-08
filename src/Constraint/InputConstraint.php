9ed9e7c5b983dd9c200dffc34be6ea3bbb041b26
Frank Dekker <fdekker@gmail.com>
2023-12-08 16:12:12 +0100

    Update package versions (#9)

--- src/Constraint/InputConstraint.php
+++ src/Constraint/InputConstraint.php
@@ -9,8 +9,7 @@ class InputConstraint extends Constraint
 {
     public const WRONG_VALUE_TYPE = '67ff49d5-9a61-47ad-80f1-960fd2beab6f';
 
-    /** @var mixed */
-    protected static $errorNames = [self::WRONG_VALUE_TYPE => 'WRONG_VALUE_TYPE',];
+    protected const ERROR_NAMES = [self::WRONG_VALUE_TYPE => 'WRONG_VALUE_TYPE',];
 
     public string $wrongTypeMessage = 'Expect value to be of type Symfony\Component\Console\Input\InputInterface';
 
@@ -26,7 +25,7 @@ class InputConstraint extends Constraint
      *     options?: Constraint|Constraint[]
      * }|null $options
      */
-    public function __construct($options = null)
+    public function __construct(?array $options = null)
     {
         // make sure defaults are set
         $options              = $options ?? [];
